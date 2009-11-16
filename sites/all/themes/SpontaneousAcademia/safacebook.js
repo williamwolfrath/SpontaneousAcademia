be769c978cf52423e4ac779338c81722abb15051
William Wolfrath <gemiinii@web18.webfaction.com>
2009-11-16 16:18:23 -0600

    Added FB stuff to load for every page

---
+++ sites/all/themes/SpontaneousAcademia/safacebook.js
@@ -0,0 +1,9 @@
+function facebookInit() {
+    FB.init('990f0319a7449a516ee2032d33478742', "xd_receiver.htm", {"reloadIfSessionStateChanged":true});
+}
+
+jQuery(document).ready(function($){
+   
+    facebookInit();
+    
+});
