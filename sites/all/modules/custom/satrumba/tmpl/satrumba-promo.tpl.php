b1054f349559b75fa8c594bc008e454a65847d9f
William Wolfrath <gemiinii@web18.webfaction.com>
2009-11-29 16:18:44 -0600

    Initial code for custom Trumba support

---
+++ sites/all/modules/custom/satrumba/tmpl/satrumba-promo.tpl.php
@@ -0,0 +1,19 @@
+<?php
+// main page for Trumba calendar functionality
+?>
+
+
+<div id="trumba-promo">
+    
+    <script type="text/javascript">
+        $Trumba.addSpud({
+        webName: "first-top-level-calendar",
+        spudType : "upcoming" ,
+        teaserBase : "http://sa.wilwolf.com/trumba-test" });
+    </script>
+    <noscript>Your browser must support JavaScript to view this content. 
+        Please enable JavaScript in your browser settings then try again. 
+    <a href='http://www.trumba.com'>Events calendar powered by Trumba</a></noscript>
+    
+</div>
+
---
+++ sites/all/modules/custom/satrumba/tmpl/satrumba.tpl.php
@@ -0,0 +1,25 @@
+<?php
+// main page for Trumba calendar functionality
+?>
+
+
+<div id="trumba">
+    
+    <script type="text/javascript">
+        $Trumba.addSpud({
+            webName: "first-top-level-calendar",
+            spudType : "chooser" });
+    </script>
+    
+    <script type="text/javascript">
+        $Trumba.addSpud({
+            webName: "first-top-level-calendar",
+            spudType : "main" });
+    </script>
+    <noscript>Your browser must support JavaScript to view this content. 
+        Please enable JavaScript in your browser settings then try again. 
+        <a href='http://www.trumba.com'>Events calendar powered by Trumba</a>
+    </noscript>
+    
+</div>
+
