2a1b1895e2c9b56b5bcc2d1ed00277883320595e
William Wolfrath <william.wolfrath@gmail.com>
2010-07-09 21:39:50 -0500

    Custom openid javascript needed
    I hate to do this this way, but we needed to remove the username and password fields on the login page. They wanted only outside ways like FB and Google to be the ways to login. However, Openid operates on those fields via Javascript, and I had to slightly alter it. Should come up with a way to do this better.

--- modules/openid/openid.js
+++ modules/openid/openid.js
@@ -1,21 +1,26 @@
 // $Id: openid.js,v 1.6 2008/01/30 22:11:22 goba Exp $
 
 Drupal.behaviors.openid = function (context) {
-  var $loginElements = $("#edit-name-wrapper, #edit-pass-wrapper, li.openid-link");
-  var $openidElements = $("#edit-openid-identifier-wrapper, li.user-link");
+  //var $loginElements = $("#edit-name-wrapper, #edit-pass-wrapper, li.openid-link");
+  var $openidElements = $("#edit-openid-identifier-wrapper, li.user-link, #edit-submit-login");
+
+  var $loginElements = $("#edit-name-wrapper, #edit-pass-wrapper");
+  var $oLink = $("li.openid-link");
 
   // This behavior attaches by ID, so is only valid once on a page.
   if (!$("#edit-openid-identifier.openid-processed").size() && $("#edit-openid-identifier").val()) {
     $("#edit-openid-identifier").addClass('openid-processed');
     $loginElements.hide();
+
     // Use .css("display", "block") instead of .show() to be Konqueror friendly.
     $openidElements.css("display", "block");
   }
   $("li.openid-link:not(.openid-processed)", context)
     .addClass('openid-processed')
     .click( function() {
-       $loginElements.hide();
-       $openidElements.css("display", "block");
+      $loginElements.hide();
+      $openidElements.css("display", "block");
+      $oLink.hide();
       // Remove possible error message.
       $("#edit-name, #edit-pass").removeClass("error");
       $("div.messages.error").hide();
@@ -27,7 +32,8 @@ Drupal.behaviors.openid = function (context) {
     .addClass('openid-processed')
     .click(function() {
        $openidElements.hide();
-       $loginElements.css("display", "block");
+       $oLink.css("display", "block");
+       //$loginElements.css("display", "block");
       // Clear OpenID Identifier field and remove possible error message.
       $("#edit-openid-identifier").val('').removeClass("error");
       $("div.messages.error").css("display", "block");
