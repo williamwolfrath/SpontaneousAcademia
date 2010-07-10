3e878a0bc648b60917914e8807208a72a6955da7
William Wolfrath <william.wolfrath@gmail.com>
2010-07-09 21:36:25 -0500

    Google Federated Open ID integration
    Added lightopenid library and php files. The googleauth one is a modification of the example-google one. Putting this logic in a module was causing some strange issues of not logging the user in correctly, so for now, I'm using the modified php file and redirecting to a module for logging in the user.
    Also added the google login button.

---
+++ lightopenid/googleauth.php
@@ -0,0 +1,34 @@
+<?php
+# Logging in with Google accounts requires setting special identity, so this example shows how to do it.
+require 'openid.php';
+try {
+    if(!isset($_GET['openid_mode'])) {
+        if(isset($_GET['login'])) {
+            $openid = new LightOpenID;
+            $openid->identity = 'https://www.google.com/accounts/o8/id';
+            $openid->required = array('namePerson/friendly', 'contact/email');
+            header('Location: ' . $openid->authUrl());
+        }
+?>
+<form action="?login" method="post">
+    <button>Login with Google</button>
+</form>
+<?php
+    } elseif($_GET['openid_mode'] == 'cancel') {
+        echo 'User has canceled authentication!';
+    } else {
+        $openid = new LightOpenID;
+        //echo 'validate: ' . $openid->validate() . "<br/>";
+        //echo 'User ' . ($openid->validate() ? $_GET['openid_identity'] . ' has ' : 'has not ') . 'logged in.';
+        if ($openid->validate()) {
+            //header('Location: ' . '/user');
+            $attrs = $openid->getAttributes();
+            $email = $attrs['contact/email'];
+            //echo ('email: ' . $email);
+            // log in person with email
+            header('Location: ' . '/gauth?gauth=' . $email);
+        }
+    }
+} catch(ErrorException $e) {
+    echo $e->getMessage();
+}
