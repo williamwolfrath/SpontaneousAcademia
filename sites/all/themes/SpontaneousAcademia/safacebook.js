5635ef4de5aabb715085dc6baf1858702dafdff3
William Wolfrath <william.wolfrath@gmail.com>
2010-08-19 22:32:54 -0500

    Removed FB attachment to login links

--- sites/all/themes/SpontaneousAcademia/safacebook.js
+++ sites/all/themes/SpontaneousAcademia/safacebook.js
@@ -1,10 +1,10 @@
 function facebookInit() {
     // change login links to do a Facebook login
-    $('a[href^=/user/login]')
-        .click(function() {
-            FB.Connect.requireSession();
-            return false;  
-        });
+    //$('a[href^=/user/login]')
+    //    .click(function() {
+    //        FB.Connect.requireSession();
+    //        return false;  
+    //    });
         
     //$('a[href^=/user/register]')
     //    .click(function() {
